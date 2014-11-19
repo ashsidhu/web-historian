523ff7f5e1df8999508e69ab25535cfb5ccd13da
ashsidhu <ashmeet.sidhu7@gmail.com>
2014-11-19 15:59:27 -0800

    serve static files

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -7,13 +7,15 @@ exports.headers = headers = {
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10, // Seconds.
-  'Content-Type': "text/html"
+  "Content-Type": "text/html"
 };
 
 exports.serveAssets = function(res, asset, callback) {
+  var basePath = '/Users/student/2014-10-web-historian/web/public';
+  fs.readFile(basePath + asset, callback);
+};
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
-};
 
 
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,34 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var httpHelpers = require('./http-helpers');
+
+
+
+var routes = {
+  staticHandler: function(req, res) {
+    httpHelpers.serveAssets(res, req.url, function (err, data) {
+      if (err) throw err;
+      res.setHeader("Content-Type", staticExt[path.extname(req.url)]);
+      res.writeHead(200, httpHelpers.headers);
+      res.end(data);
+    });
+  }
+};
+
+var staticExt = {
+  '.html': "text/html",
+  '.css': "text/css"
+};
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.url === '/') {
+    req.url = '/index.html';
+  }
+
+  if (staticExt[path.extname(req.url)]) {
+    routes.staticHandler(req, res);
+  }
+
+  // res.end(archive.paths.list);
 };
